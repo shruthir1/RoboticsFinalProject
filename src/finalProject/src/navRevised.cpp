825d582c126cdc453a65f08d90bb32428dce15ac
shruthir1 <166077380+shruthir1@users.noreply.github.com>
2025-12-08 13:50:32 -0800

    A lot more developed now!!

--- src/finalProject/src/navRevised.cpp
+++ src/finalProject/src/navRevised.cpp
@@ -1,5 +1,5 @@
 #include <rclcpp/rclcpp.hpp> 
-#include <navigation/navigation.hpp>
+#include <nav.hpp>
 #include <iostream>
 //make an array of all x and y coordinates, use for loop for exery x all y's and for every y all x's in this for loop we call the waypoints function. 
 //Use if statements if above or below max/min values, handle accordingly 
@@ -10,29 +10,27 @@ public:
         // initialize Navigator inside constructor
     }
 
+    int allX[] = {};
+    int allY[63] = {-38, -37, -36, -35, -34, -33, -32, -31, -30, -29, -28, -27, -26, -25, -24, -23, -22, -21, -20, -19, -18, -17, -16, -15, -14, -13, -12, -11, -10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};
+    int humanCount = 0;
+    bool humanFound = false;
+    int finalHuman1x = 0;
+    int finalHuman1y = 0;
+    int finalHuman2x = 0;
+    int finalHuman2y = 0;
+
 private:
-    // waypoints, timers, callback functions go here
-    Navigator navigator_;
-};
+   void initializePose(){
+     // first: it is mandatory to initialize the pose of the robot
+        geometry_msgs::msg::Pose::SharedPtr init = std::make_shared<geometry_msgs::msg::Pose>();
+        init->position.x = 2.12;
+        init->position.y = -21.3;
+        init->orientation.w = 1.53;
+        navigator.SetInitialPose(init);
+    
+   }
 
-int main(int argc,char **argv) {
- 
-  rclcpp::init(argc,argv); // initialize ROS 
-  Navigator navigator(true,false); // create node with debug info but not verbose
-  
-  // first: it is mandatory to initialize the pose of the robot
-  geometry_msgs::msg::Pose::SharedPtr init = std::make_shared<geometry_msgs::msg::Pose>();
-  init->position.x = 2.12;
-  init->position.y = -21.3;
-  init->orientation.w = 1.53;
-  navigator.SetInitialPose(init);
-  // spin in place of 90 degrees (default parameter)
-  //if we go to intentded position we want to spin 360 and then continue moving
-  while ( ! navigator.IsTaskComplete() ) {
-    // busy waiting for task to be completed
-  }
-
-  void newWaypoints(int x, int y, int w){
+   void newWaypoints(int x, int y, int w){
     //sping 360 at the start of every waypoint 
       navigator.Spin();
       navigator.Spin();
@@ -49,7 +47,22 @@ int main(int argc,char **argv) {
       //if(humanCount != 2){
           //humanFound = false;
   //}
-      
+
+    if(humanFound){ 
+        if(humanCount !=2){
+            finalHuman1x = x;
+            finalHuman1y = y;
+            humanFound = false;
+            RCLCPP_INFO(this->get_logger(), " Found Human 1 Around: (%d, %d)\n", finalHuman1x, finalHuman1y);
+        }
+
+        finalHuman2x = x;
+        finalHuman2y = y;
+        RCLCPP_INFO(this->get_logger(), "Found Human 2 Around: (%d, %d)\n", finalHuman2x, finalHuman2y);
+        
+
+    }
+     
     geometry_msgs::msg::Pose::SharedPtr goal_pos = std::make_shared<geometry_msgs::msg::Pose>();
     if(/*if reached a maximum/minimum X or Y*/){
          goal_pos->position.x = x;
@@ -64,25 +77,24 @@ int main(int argc,char **argv) {
     //moving to a new pose
     navigator.GoToPose(goal_pos);
   }
+};
+
+int main(int argc,char **argv) {
+ 
+  rclcpp::init(argc,argv); // initialize ROS 
+  Navigator navigator(true,false); // create node with debug info but not verbose
+  
+  //if we go to intentded position we want to spin 360 and then continue moving
+  while ( ! navigator.IsTaskComplete() ) {
+    // busy waiting for task to be completed
+  }
+
 
   while (!navigator.IsTaskComplete()) {
     return; 
   }
-
-  //go to new pose again 
-  goal_pos->position.x = 2;
-  goal_pos->position.y = -1;
-  goal_pos->orientation.w = 1;
-  navigator.GoToPose(goal_pos);
-  // move to new pose
-
-  while ( ! navigator.IsTaskComplete() ) {
-    //busy doing another task
-    return;
-  }
-
-  // complete here....
   
+  rclcpp::spin(); // here the spin statement needs to wait for whether human was found info from perception, if not found keep calling relevant functions 
   rclcpp::shutdown(); // shutdown ROS
   return 0;
 }
